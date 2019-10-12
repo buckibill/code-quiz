bf8c714ef26bebd123bbbd90af2758d850cea382
William Murray <buckibill@Williams-MacBook-Pro.local>
2019-10-11 21:59:19 -0700

    inital commit

---
+++ questions.js
@@ -0,0 +1,29 @@
+var questions = [
+
+    {
+        question: 'What computer program do developers use to write code?',
+        choices: ['Visual Studio Code', 'Excel','Quatro Pro', 'Words with Codes'],
+        correctAnswer: 'Visual Studio Code',
+    },
+    {
+        question: 'What coding lanquages makeup generally most webpages?',
+        choices: ['HTML','CSS','Javascript','All of the Above'],
+        correctAnswer: 'All of the Above',
+    },
+    {
+        question: 'What does SQL stand for?',
+        choices: ['Start Question Language','Structured Query Language','Standard Query Language','None of these answers are correct'],
+        correctAnswer: 'Structured Query Language',
+    },
+    {
+        question: 'What language do you use to style web pages?',
+        choices: ['Python', 'C++','CSS','HTML'],
+        correctAnswer: 'CSS',
+    },
+    {
+        question: 'What does Hyper Text Markup Language stand for?',
+        choices: ['CSS', 'Javascript', 'HTML','CSS'],
+        correctAnswer: 'HTML',
+    }
+
+]
